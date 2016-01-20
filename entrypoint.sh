8e4d655047cf846f06c10e66a124e323a15a1fd8
Tomohisa Kusano <siomiz@gmail.com>
2016-01-20 23:30:49 +0000

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [ ! -f /home/bitcoin/.bitcoin/bitcoin.conf ]; then
+
+mkdir -p /home/bitcoin/.bitcoin
+
+/usr/bin/bitcoind 2>&1 | /bin/grep ^rpc > /home/bitcoin/.bitcoin/bitcoin.conf
+
+exec "$@"
+
+fi
